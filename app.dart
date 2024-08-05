4cab93dbbd8325c3ecc43017244bf0216b271903
Jennifer <jennifer15aries@gmail.com>
2024-08-04 19:46:49 -0500

    extends

--- app.dart
+++ app.dart
@@ -1,7 +1,47 @@
+//Clase abstracta no se puede instanciar
 void main(){
-  print('The sum is : ${addition(10,20)}');
+  final windPlant = WindPlant(initialEnergy:100);
+  final nuclearPlant = NuclearPlant();
+  
+  print('wind: ${chargePhone(windPlant)}');
+  print('nuclear: ${chargePhone(nuclearPlant)}');
 }
 
-int addition(int a, int b){
-  return a + b;
+//funcion
+double chargePhone(EnergyPlant plant){
+  if(plant.energyLeft < 10){
+    throw Exception('Not enough energy');
+  }
+  
+  return plant.energyLeft -10;
+}
+
+//enumeración -> nuevo tipo de planta
+enum PlantType {nuclear,wind,water}
+
+abstract class EnergyPlant{
+  double energyLeft;
+  PlantType type; // nuclear , wind, water
+  
+  //constructor
+  EnergyPlant({required this.energyLeft, required this.type});
+  
+  void consumeEnergy( double amount);
+}
+//Herencia
+class WindPlant extends EnergyPlant{
+  //constructor
+  WindPlant({ required double initialEnergy }):
+  //constructor del padre
+  super(energyLeft : initialEnergy, type:PlantType.wind);
+  
+  @override
+  void consumeEnergy( double amount){
+    energyLeft -= amount;
+  }
+}
+
+//Implementar
+class NuclearPlant implements EnergyPlant{
+  
 }
